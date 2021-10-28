965f0239ea0cc4dbf416a5e99f0d247a47ae9514
Aditya Singhal <adityaarunsinghal@gmail.com>
2021-10-28 09:46:01 -0400

    Adding setup script to gameplay

--- Object Physics Sandbox/Assets/SaveAndResetPositions.cs
+++ Object Physics Sandbox/Assets/SaveAndResetPositions.cs
@@ -7,10 +7,11 @@ public class SaveAndResetPositions : MonoBehaviour
     bool inSetupPhase = true;
     private Vector2[] objectPositions;
     private float[] gravities;
-    private DragDrop3[] saveableObjects = FindObjectsOfType<DragDrop3>();
+    private DragDrop3[] saveableObjects;
 
     void Awake()
     {
+        saveableObjects = FindObjectsOfType<DragDrop3>();
         objectPositions = new Vector2[saveableObjects.Length];
     }
     public void onButtonPress()
@@ -20,8 +21,8 @@ public class SaveAndResetPositions : MonoBehaviour
             // reset positions and turn off gravity
             for (int i = 0; i < saveableObjects.Length; i++)
             {
-                gravities[i] = saveableObjects[i].gravityScale;
-                saveableObjects[i].gravityScale = 0.0f;
+                gravities[i] = saveableObjects[i].GetComponent <Rigidbody2D>().gravityScale;
+                saveableObjects[i].GetComponent <Rigidbody2D>().gravityScale = 0.0f;
                 saveableObjects[i].transform.position = objectPositions[i];
             }
             inSetupPhase = !inSetupPhase;
@@ -32,7 +33,7 @@ public class SaveAndResetPositions : MonoBehaviour
             // save setup and turn on game play
             for (int i = 0; i < objectPositions.Length; i++)
             {
-                saveableObjects[i].gravityScale = gravities[i];
+                saveableObjects[i].GetComponent <Rigidbody2D>().gravityScale = gravities[i];
                 objectPositions[i] = saveableObjects[i].transform.position;
             }
             inSetupPhase = !inSetupPhase;
