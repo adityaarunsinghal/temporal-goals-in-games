5d092d6d46a919b2efee448ebf49a60a04c452bf
Aditya Singhal <adityaarunsinghal@gmail.com>
2022-02-11 00:10:55 +0530

    i am so stupid - the right wall collider was overlapping on all objects...

--- Object Physics Sandbox/Assets/dragDrop/DragDrop.cs
+++ Object Physics Sandbox/Assets/dragDrop/DragDrop.cs
@@ -7,31 +7,33 @@ public class DragDrop : MonoBehaviour // classic free moving of objects
     public Rigidbody2D selectedObject;
     Vector3 mousePosition;
 
-    void Update()
-    {
-        // track mouse
-        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        // Select object 
-        if (Input.GetMouseButtonDown(0))
-        {
-            Collider2D targetObject = Physics2D.OverlapPoint(mousePosition);
-
-            // if something was actually clicked
-            if (targetObject)
-            {
-                if (targetObject.gameObject.tag != "ball")
-                {
-                    selectedObject = targetObject.gameObject.GetComponent<Rigidbody2D>();
-                }
-            }
-        }
-    }
-
-    // void OnMouseDown()
+    // void Update()
     // {
-    //     selectedObject = GetComponent<Rigidbody2D>();
+    //     // track mouse
+    //     mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+    //     // Select object 
+    //     if (Input.GetMouseButtonDown(0))
+    //     {
+    //         Collider2D targetObject = Physics2D.OverlapPoint(mousePosition);
+
+    //         // if something was actually clicked
+    //         if (targetObject)
+    //         {
+    //             if (targetObject.gameObject.tag != "ball")
+    //             {
+    //                 selectedObject = targetObject.gameObject.GetComponent<Rigidbody2D>();
+    //                 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //                 selectedObject.position = mousePosition;
+    //             }
+    //         }
+    //     }
     // }
+
+    void OnMouseDown()
+    {
+        selectedObject = GetComponent<Rigidbody2D>();
+    }
 
     void OnMouseDrag()
     {
