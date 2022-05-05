69a6a725f9a1721aceb68acd69e4ec2d021ceb56
Aditya <adityaarunsinghal@gmail.com>
2022-05-05 15:18:14 -0400

    set off game 123 learnability

--- Code/GAME_2_LEARNER.py
+++ Code/GAME_2_LEARNER.py
@@ -1,4 +1,4 @@
-from GAME_1_SOLVER import *
+from GAME_2_SOLVER import *
 from stable_baselines import PPO2
 from stable_baselines.bench.monitor import Monitor
 from mlagents_envs.side_channel.engine_configuration_channel import EngineConfigurationChannel
@@ -15,20 +15,20 @@ if __name__ == "__main__":
     channel = EngineConfigurationChannel()
     channel.set_configuration_parameters(time_scale=50, quality_level=0)
     unity_env = UnityEnvironment(
-        file_name=SERVER_BUILD, seed=1, side_channels=[channel], worker_id=1)
+        file_name=SERVER_BUILD, seed=1, side_channels=[channel], worker_id=2)
 
     # Start interacting with the environment.
     unity_env.reset()
     gym_env = UnityToGymWrapper(unity_env, allow_multiple_obs=False)
-    env = PlaceAndShootGym(gym_env, reward_fn=GAME_1_REWARD,
-                           actionTransformer=GAME_1_TRANSFORMER,
+    env = PlaceAndShootGym(gym_env, reward_fn=GAME_2_REWARD,
+                           actionTransformer=GAME_2_TRANSFORMER,
                            announce_actions=False)
 
     print("GYM READY")
 
-    env.setup(GAME_1_SETUP)
+    env.setup(GAME_2_SETUP)
 
-    run_name = "/scratch/as11919/temporal-goals-in-games/Code/results/GAME_1/PPO2_GAME_1"
+    run_name = "/scratch/as11919/temporal-goals-in-games/Code/results/GAME_2/PPO2_GAME_2"
     monitored_env = Monitor(env=env, filename=run_name,
                             allow_early_resets=True)
 
---
+++ Code/sbatch_scripts/GAME_1_learnability.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+
+#SBATCH --job-name=game1
+#SBATCH --nodes=1
+#SBATCH --open-mode=append
+#SBATCH --output=./slurm_logs/%j_game1.out
+#SBATCH --error=./slurm_logs/%j_game1.err
+#SBATCH --export=ALL
+#SBATCH --cpus-per-task=4 
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=64G
+#SBATCH --time=20:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=ALL
+#SBATCH -c 8
+#SBATCH --mail-user=adis@nyu.edu
+
+singularity exec --nv --overlay $SCRATCH/overlay-50G-10M.ext3:ro /scratch/work/public/singularity/cuda11.0-cudnn8-devel-ubuntu18.04.sif /bin/bash -c '
+
+source /ext3/env.sh
+conda activate tf_baselines_games
+
+echo ">>>> starting evals"
+
+python3 /scratch/as11919/temporal-goals-in-games/Code/GAME_1_LEARNER.py
+
+echo ">>>> done with evals"
+
+'
